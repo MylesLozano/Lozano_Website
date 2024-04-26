03375b640565e2b1a8885d137ffdced64f75fdf5
ADMIN <humphrey.lozano@jmc.edu.ph>
2024-04-26 18:24:02 +0800

    Added items to show.blade, currently static

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Spotlight Products</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/Spotlight Shop.png') }}">
+
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-6">
+            <img src="{{ asset('images/Nanite Hoodies.png') }}" class="d-block w-100" alt="Product 1">
+        </div>
+        <div class="col-md-5 pt-3">
+            <h5>Product 1</h5>
+            <p>LozanoTech's newest fashionable jacket hoodie! Style yourself with the latest
+                clothing inspired by LozanoTech's NanoSuit V5, hailed as an industry standard in
+                nanotechnology armor and fabric protection!</p>
+            <h4>Product Features:</h4>
+            <ul>
+                <li>Made from 100% LozanoTech Nanites for shock and physical trauma protection.</li>
+                <li>Self-repairing ability rivaling the NanoSuit V2.</li>
+                <li>Automatic temperature control module for the best comfort possible.</li>
+            </ul>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
